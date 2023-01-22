3951fa5c6931d57cf16d9a466e81d162ba1c2421
SaimAli2002 <123175928+SaimAli2002@users.noreply.github.com>
2023-01-22 12:02:14 +0500

    Add files via upload

---
+++ Collections.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+point = namedtuple('point','x,y')
+pt=point(2,-8)
+print(pt.x,pt.y)
+
+# If you want to run the code below Comment the above code and uncomment the code below
+
+#from collections import OrderedDict
+#Ordered_Dict=OrderedDict()
+#Ordered_Dict['a']=1
+#Ordered_Dict['b']=6
+#Ordered_Dict['c']=4
+#Ordered_Dict['d']=3
+#Ordered_Dict['e']=2
+#print(Ordered_Dict)
